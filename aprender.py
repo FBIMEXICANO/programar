3a4e174e8cc6e8a06a18547d0847af259823f33b
FBIMEXICANO <gerardo_lh04@outlook.es>
2022-11-23 11:24:16 -0600

    agregar

---
+++ aprender.py
@@ -0,0 +1,2 @@
+print("hola como estas espero que estes bien bastante bien hoy te presento")
+print("aprender")
---
+++ ingresa.py
@@ -0,0 +1,4 @@
+num1=int(input("ingresa emayor numero 1: "))
+num2=int(input("ingresa emayor numero 2: "))
+num3=int(input("ingresa emayor numero 3: "))
+
